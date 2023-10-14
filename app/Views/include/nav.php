5100bf9490d0dcc389b82ede540fc47a500535a2
Wesley <wesley.santos140300@gmail.com>
2023-10-14 20:56:30 -0300

    feat: script instagram e HTML da nav

--- app/Views/include/nav.php
+++ app/Views/include/nav.php
@@ -1,1 +1,152 @@
-<h1>Sou a nav</h1>
+<header>
+    <div class="text-center py-3 header-top">
+        <span><b>SONHE, PLANEJE E REALIZE!</b></span>
+    </div>
+    <div class="border-header"></div>
+
+    <div class="container d-flex justify-content-between align-items-center my-4">
+        <form action="" action="" class="w-25 form-search-nav" style="position: relative">
+            <input type="search" class="input-navegacao" name="pesquisa-nav" placeholder="Buscar">
+            <img src="assets/icons/lupa.png" style="position: absolute; top: 50%; right: 120px; transform: translateY(-50%); height: 20px; width: 20px;" alt="Ícone Search" />
+        </form>
+
+        <div class="w-50 text-center box-logo-header">
+            <img class="" src="assets/img/logo-projeto.png" alt="">
+        </div>
+
+        <div class="d-flex w-25 justify-content-end content-icons-nav content-icons-nav-desktop">
+            <a href="" class="d-flex flex-column align-items-center">
+                <img src="assets/icons/carrinho-compras.png" alt="" class="icon-nav" style="width: 32px">
+                carrinho
+            </a>
+            <!-- SEM LOGIN -->
+            <a href="" class="d-flex flex-column align-items-center">
+                <img src="assets/icons/user.png" alt=""  class="icon-nav" style="width: 32px">
+                iniciar sessão
+            </a>
+
+            <!-- COM LOGIN -->
+            <!-- <a href="" class="d-flex flex-column align-items-center">
+                <img src="assets/icons/user.png" alt=""  class="icon-nav" style="width: 32px">
+                profile
+            </a> -->
+        </div>
+    </div>
+
+    <header class="header">
+        <div class="container">
+            <div class="wrapper">
+                <div class="header-item-left  content-icons-nav content-icons-nav-mobile">
+                    <!-- por ícone projeto no mobile (apenas) -->
+                    <!-- <a href="#" class="brand"><img src="assets/img/logo-projeto.png" alt="" style="width: 120px;"></a> -->
+                    <a href="" class="d-flex flex-column align-items-center">
+                        <img src="assets/icons/carrinho-compras.png" alt="" class="icon-nav" style="width: 32px">
+                        carrinho
+                    </a>
+                    <!-- SEM LOGIN -->
+                    <a href="" class="d-flex flex-column align-items-center">
+                        <img src="assets/icons/user.png" alt=""  class="icon-nav" style="width: 32px">
+                        iniciar sessão
+                    </a>
+
+                    <!-- COM LOGIN -->
+                    <!-- <a href="" class="d-flex flex-column align-items-center">
+                        <img src="assets/icons/user.png" alt=""  class="icon-nav" style="width: 32px">
+                        profile
+                    </a> -->
+                </div>
+                <div class="header-item-center">
+                    <div class="overlay"></div>
+                    <nav class="menu">
+                        <div class="menu-mobile-header">
+                            <!-- por icone menu -->
+                            <button type="button" class="menu-mobile-arrow">
+                                <img src="assets/icons/back.png" alt="" style="width: 32px;">
+                            </button>
+                            <div class="menu-mobile-title"></div>
+                                <button type="button" class="menu-mobile-close">
+                                    <!-- <i class="ion ion-ios-close"></i> -->
+                                    <img src="assets/icons/close.png" alt="" style="width: 32px;">
+                                </button>
+                        </div>
+                        <ul class="menu-section menu-nav">
+                            <!-- <li><a href="#">Home</a></li> -->
+
+                            <li class="menu-item-has-children">
+                                <a href="#">PLANNERS<img src="#" alt=""></a>
+                                <div class="menu-subs menu-column-1">
+                                    <ul>
+                                        <li><a href="#">Item 01</a></li>
+                                        <li><a href="#">Item 02</a></li>
+                                        <li><a href="#">Item 03</a></li>
+                                        <li><a href="#">Item 04</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+
+                            <li class="menu-item-has-children">
+                                <a href="#">AGENDA<img src="#" alt=""></a>
+                                <div class="menu-subs menu-column-1">
+                                    <ul>
+                                        <li><a href="#">Item 01</a></li>
+                                        <li><a href="#">Item 02</a></li>
+                                        <li><a href="#">Item 03</a></li>
+                                        <li><a href="#">Item 04</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+
+                            <li class="menu-item-has-children">
+                                <a href="#">BLOCO DE NOTAS<img src="#" alt=""></a>
+                                <div class="menu-subs menu-column-1">
+                                    <ul>
+                                        <li><a href="#">Item 01</a></li>
+                                        <li><a href="#">Item 02</a></li>
+                                        <li><a href="#">Item 03</a></li>
+                                        <li><a href="#">Item 04</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+
+                            <li class="menu-item-has-children">
+                                <a href="#">CADERNOS<img src="#" alt=""></a>
+                                <div class="menu-subs menu-column-1">
+                                    <ul>
+                                        <li><a href="#">Item 01</a></li>
+                                        <li><a href="#">Item 02</a></li>
+                                        <li><a href="#">Item 03</a></li>
+                                        <li><a href="#">Item 04</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+
+                            <li class="menu-item-has-children">
+                                <a href="#">PRONTA ENTREGA<img src="#" alt=""></a>
+                                <div class="menu-subs menu-column-1">
+                                    <ul>
+                                        <li><a href="#">Item 01</a></li>
+                                        <li><a href="#">Item 02</a></li>
+                                        <li><a href="#">Item 03</a></li>
+                                        <li><a href="#">Item 04</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+
+                        </ul>
+                    </nav>
+                </div>
+                <div class="header-item-right">
+                    <a href="#" class="menu-icon"><i class="ion ion-md-search"></i></a>
+                    <a href="#" class="menu-icon"><i class="ion ion-md-heart"></i></a>
+                    <a href="#" class="menu-icon"><i class="ion ion-md-cart"></i></a>
+                    <button type="button" class="menu-mobile-trigger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+              </button>
+                </div>
+            </div>
+        </div>
+    </header>
+</header>
---
+++ public/assets/js/nav.js
@@ -0,0 +1,58 @@
+const menu = document.querySelector('.menu');
+const menuSection = menu.querySelector('.menu-section');
+const menuArrow = menu.querySelector('.menu-mobile-arrow');
+const menuClosed = menu.querySelector('.menu-mobile-close');
+const menuTrigger = document.querySelector('.menu-mobile-trigger');
+const menuOverlay = document.querySelector('.overlay');
+let subMenu;
+menuSection.addEventListener('click', (e) => {
+    if (!menu.classList.contains('active')) {
+        return;
+    }
+    if (e.target.closest('.menu-item-has-children')) {
+        const hasChildren = e.target.closest('.menu-item-has-children');
+        showSubMenu(hasChildren);
+    }
+});
+menuArrow.addEventListener('click', () => {
+    hideSubMenu();
+});
+menuTrigger.addEventListener('click', () => {
+    toggleMenu();
+});
+menuClosed.addEventListener('click', () => {
+    toggleMenu();
+});
+menuOverlay.addEventListener('click', () => {
+    toggleMenu();
+});
+
+function toggleMenu() {
+    menu.classList.toggle('active');
+    menuOverlay.classList.toggle('active');
+}
+
+function showSubMenu(hasChildren) {
+    subMenu = hasChildren.querySelector('.menu-subs');
+    subMenu.classList.add('active');
+    subMenu.style.animation = 'slideLeft 0.5s ease forwards';
+    const menuTitle = hasChildren.querySelector('i').parentNode.childNodes[0].textContent;
+    menu.querySelector('.menu-mobile-title').innerHTML = menuTitle;
+    menu.querySelector('.menu-mobile-header').classList.add('active');
+}
+
+function hideSubMenu() {
+    subMenu.style.animation = 'slideRight 0.5s ease forwards';
+    setTimeout(() => {
+        subMenu.classList.remove('active');
+    }, 300);
+    menu.querySelector('.menu-mobile-title').innerHTML = '';
+    menu.querySelector('.menu-mobile-header').classList.remove('active');
+}
+window.onresize = function() {
+    if (this.innerWidth > 991) {
+        if (menu.classList.contains('active')) {
+            toggleMenu();
+        }
+    }
+};
