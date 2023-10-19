d4a856088db08a2d508f27fb0995d28955ec2a25
_welao <74609771+WesleyRodrigues55@users.noreply.github.com>
2023-10-19 12:30:04 -0300

    Revert "feat: criação tela cadastro produto adm"

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -19,15 +19,6 @@ class User extends BaseController
             view('/login/cadastrar');
     }
 
-    //============================================================================
-    # ADMINISTRADOR 
-    public function cadastroAdm()
-    {
-        return
-            view('/adm/cadastro-adm');
-    }
-
-
 
     //============================================================================
     # LOGOUT
@@ -63,14 +54,14 @@ class User extends BaseController
             return redirect()->to('usuario/login?error'); //pesquisar sobre erro   --------------------------------------------------------
         } else {
             session()->set([
-                'id' => $query[0]['ID'],
-                'usuario' => $query[0]['USUARIO'],
-                'pessoa_id' => $query[0]['PESSOA_ID'],
-                'nivel' => $query[0]['NIVEL'],
-                'ativo' => $query[0]['ATIVO'],
-            ]);
+            'id' => $query[0]['ID'],
+            'usuario' => $query[0]['USUARIO'],
+            'pessoa_id' => $query[0]['PESSOA_ID'],
+            'nivel' => $query[0]['NIVEL'],
+            'ativo' => $query[0]['ATIVO'],
+        ]);
         }
-
+        
         // Redireciona com base no nível
         if (session()->get('nivel') == 1) {
             return redirect()->to('../');
@@ -78,7 +69,7 @@ class User extends BaseController
             return redirect()->to('pagina-de-administrador');
         }
         // print_r(session()->get());
-
+    
 
     }
 
@@ -87,5 +78,6 @@ class User extends BaseController
     public function esqueceuSenha(): string
     {
         return view('login/esqueci-senha');
+
     }
 }
