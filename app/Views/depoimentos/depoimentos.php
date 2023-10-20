c116f21c10787a2839531f6e3539a7af33c93e94
Marilia <marilialloureiro@hotmail.com>
2023-10-20 16:22:17 -0300

    feat: ajustes no input - inserção do atributo name

--- app/Views/depoimentos/depoimentos.php
+++ app/Views/depoimentos/depoimentos.php
@@ -23,52 +23,53 @@
                 nossos corações enchem de alegria.</p>
         </div>
 
-        <div class="row">
-            <div class=" col-md-6">
-                <label for="nome" class="preencher">NOME*</label>
-                <input type="text" class="form-control" id="nome" placeholder="Digite o seu nome" required>
-                <div class="invalid-feedback">
-                    Por favor preencha o seu nome.
+        <form class="needs-validation" novalidate>    
+            <div class="row">
+                <div class=" col-md-6">
+                    <label for="nome" class="preencher">NOME*</label>
+                    <input type="text" class="form-control" name="nome" id="nome" placeholder="Digite o seu nome" required>
+                    <div class="invalid-feedback">
+                        Por favor preencha o seu nome.
+                    </div>
+                </div>
+
+                <div class=" col-md-6">
+                    <label for="email" class="preencher">EMAIL*</label>
+                    <input type="text" class="form-control" name="email" id="email" placeholder="email@dominio.com.br" required>
+                    <div class="invalid-feedback">
+                        Por favor preencha o seu email.
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <label for="telefone" class="preencher">TELEFONE</label>
+                    <input type="text" name="telefone" class="form-control" id="telefone">
+                </div>
+
+                <div class="col-md-6">
+                    <label for="instagram" class="preencher">INSTAGRAM</label>
+                    <input type="text" class="form-control" name="instagram" id="instagram">
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <label for="name" class="preencher">MENSAGEM</label>
+                    <textarea class="form-control message-field" id="name" rows="5"></textarea>
                 </div>
             </div>
 
-            <div class=" col-md-6">
-                <label for="email" class="preencher">EMAIL*</label>
-                <input type="text" class="form-control" id="email" placeholder="email@dominio.com.br" required>
-                <div class="invalid-feedback">
-                    Por favor preencha o seu email.
-                </div>
+            <div class="col-md-12 mt-3 d-flex justify-content-end">
+                <a class="btn input-rosa px-5" href="#">ENVIAR</a></button>
             </div>
 
-            <div class="col-md-6">
-                <label for="telefone" class="preencher">TELEFONE</label>
-                <input type="text" class="form-control" id="telefone">
-            </div>
-
-            <div class="col-md-6">
-                <label for="instagram" class="preencher">INSTAGRAM</label>
-                <input type="text" class="form-control" id="instagram">
-            </div>
-
-        </div>
-
-        <div class="row">
-            <div class="col-md-12">
-                <label for="name" class="preencher">MENSAGEM</label>
-                <textarea class="form-control message-field" id="name" rows="5"></textarea>
-            </div>
-        </div>
-
-        <div class="col-md-12 mt-3 d-flex justify-content-end">
-            <a class="btn input-rosa px-5" href="#">ENVIAR</a></button>
-        </div>
-
-        <p class="text-right my-5">Também sabemos que muitas vezes imprevistos podem acontecer, se esse é o seu caso e
-            você precisa de qualquer tipo de suporte, é só enviar um e-mail para planner.bymarilia@hotmail.com ou nos
-            chamar no whatsapp que nós vamos tentar ajudar rapidinho.</p>
-            
+            <p class="text-right my-5">Também sabemos que muitas vezes imprevistos podem acontecer, se esse é o seu caso e
+                você precisa de qualquer tipo de suporte, é só enviar um e-mail para planner.bymarilia@hotmail.com ou nos
+                chamar no whatsapp que nós vamos tentar ajudar rapidinho.</p>
+        </form>     
     </div>
-
+    
 
 </main>
 
