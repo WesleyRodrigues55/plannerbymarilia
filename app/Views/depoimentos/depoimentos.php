0f48afefe67e3149222a2a689b4a349e248fa127
Marilia <marilialloureiro@hotmail.com>
2023-10-20 16:37:17 -0300

    feat: alteração na tag <textarea>

--- app/Views/depoimentos/depoimentos.php
+++ app/Views/depoimentos/depoimentos.php
@@ -56,7 +56,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <label for="name" class="preencher">MENSAGEM</label>
-                    <textarea class="form-control message-field" id="name" rows="5"></textarea>
+                    <textarea class="form-control message-field" name = "mensagem" id="name" rows="5"></textarea>
                 </div>
             </div>
 
