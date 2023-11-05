5fac9cbaa7f68958d60c7766348d67bbf54e8e82
Enrico Cattani <encattani@hotmail.com>
2023-11-05 20:30:02 -0300

    add: cpf and cnpj masks

---
+++ public/assets/js/cnpjMask.js
@@ -0,0 +1,8 @@
+function cnpj(v){
+    v=v.replace(/\D/g,"")                           //Remove tudo o que não é dígito
+    v=v.replace(/^(\d{2})(\d)/,"$1.$2")             //Coloca ponto entre o segundo e o terceiro dígitos
+    v=v.replace(/^(\d{2})\.(\d{3})(\d)/,"$1.$2.$3") //Coloca ponto entre o quinto e o sexto dígitos
+    v=v.replace(/\.(\d{3})(\d)/,".$1/$2")           //Coloca uma barra entre o oitavo e o nono dígitos
+    v=v.replace(/(\d{4})(\d)/,"$1-$2")              //Coloca um hífen depois do bloco de quatro dígitos
+    return v
+}
