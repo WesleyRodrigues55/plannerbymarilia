ca376e3580fa6933b392b48d6c1c65e7ee2beb1f
Wesley <wesley.santos140300@gmail.com>
2023-11-20 22:55:56 -0300

    ix: correção valor total pedido arredondado

--- app/Views/include/head.php
+++ app/Views/include/head.php
@@ -30,6 +30,8 @@
 
     <!-- JQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+    <?php if (base_url()) ?>
     <script src="<?= base_url("assets/js/masks.js")?>"></script>    
 </head>
 
